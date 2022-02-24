058ee0ce3e92b37f3dbfb147e9cf3ebdb2215805
DanielaReinhardt <reinhardtd@telekom.de>
2022-02-24 08:40:45 +0100

    learn jshell

---
+++ Math.java
@@ -0,0 +1,15 @@
+class Math {
+    public static void main (String[] args){
+
+        int x = 3;
+        int y = 5;
+        int z = 9;
+        int a = 8;
+    
+        System.out.println(x+y);
+        System.out.println(z-a);
+        System.out.println(y*z);
+        System.out.println(z/x);
+       
+    }
+}
